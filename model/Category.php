aad1163ac16228e1a8681bdb063d3c14de061e5b
NadarianBerija <nadarian.beria@gmail.com>
2026-01-19 11:29:53 +0200

    added translation for categories

--- model/Category.php
+++ model/Category.php
@@ -1,7 +1,9 @@
 <?php
 class Category{
     public static function getAllCategory() {
-        $query = "SELECT * FROM category";
+        $lang = getLang();
+
+        $query = "SELECT c.id, cl.name FROM category c JOIN category_lang cl ON cl.cat_id = c.id WHERE cl.lang = '$lang' ORDER BY c.id";
         $db = new Database();
         $arr = $db->getAll($query);
         return $arr;
